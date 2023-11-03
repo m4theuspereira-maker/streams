8f820df3642d2ddc8668a5ed9b990b8c8f8cfe4f
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-11-03 15:34:39 -0300

    refactor: add output types

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,13 @@ export class ProcessFile {
     });
 
     let keys: string[] = [];
-    const invoices: IInvoice[] = [];
 
     let currentLine = 0;
+    const invoicesWith0000CEP: IInvoice[] = [];
+    const invoicesWithMax6Pages: IInvoice[] = [];
+    const invoicesWithMax12Pages: IInvoice[] = [];
+    const invoicesWithMoreThan12Pages: IInvoice[] = [];
+    const invoicesWithValue0: IInvoice[] = [];
 
     reader.on("line", (line: string) => {
       if (currentLine === 0) {
@@ -23,51 +27,95 @@ export class ProcessFile {
         const invoice: any = {};
 
         for (let i = 0; i < keys.length; i++) {
-          invoice[keys[i]] = value[i];
+          invoice[keys[i]] = value[i].trim();
+          if (keys[i] === "NumeroPaginas") {
+            invoice[keys[i]] =
+              Number(value[i]) % 2 === 0
+                ? Number(value[i])
+                : Number(value[i]) + 1;
+          }
+
+          if (keys[i] === "ValorFatura") {
+            invoice[keys[i]] = Number(value[i]);
+          }
         }
 
-        invoices.push(invoice);
+        if (invoice.CEP === "00000000") {
+          invoicesWith0000CEP.push(invoice);
+        }
+
+        if (invoice.CEP !== "00000000" && invoice.ValorFatura === 0) {
+          invoicesWithValue0.push(invoice);
+        }
+
+        if (invoice.CEP !== "00000000" && invoice.NumeroPaginas <= 6) {
+          invoicesWithMax6Pages.push(invoice);
+        }
+
+        if (invoice.CEP !== "00000000" && invoice.NumeroPaginas <= 12) {
+          invoicesWithMax12Pages.push(invoice);
+        }
+
+        if (invoice.CEP !== "00000000" && invoice.NumeroPaginas > 12) {
+          invoicesWithMoreThan12Pages.push(invoice);
+        }
       }
 
       currentLine++;
     });
 
     reader.on("close", () => {
-      this.readCSVFile(outputFile, invoices);
+      Promise.all([
+        this.writeCSVFile(
+          `${outputFile}-com-CEP-zerados.csv`,
+          invoicesWith0000CEP
+        ),
+        this.writeCSVFile(
+          `${outputFile}-com-ate-6-paginas.csv`,
+          invoicesWithMax6Pages
+        ),
+        this.writeCSVFile(
+          `${outputFile}-com-ate-12-paginas.csv`,
+          invoicesWithMax12Pages
+        ),
+        this.writeCSVFile(
+          `${outputFile}-com-mais-de12-paginas.csv`,
+          invoicesWithMoreThan12Pages
+        ),
+        this.writeCSVFile(
+          `${outputFile}-com-valor-da-fatura-0.csv`,
+          invoicesWithValue0
+        )
+      ]);
+
       var duracao = Date.now() - antes;
       console.log(duracao);
     });
   }
 
-  readCSVFile(outputFileName: string, invoices: IInvoice[]) {
+  writeCSVFile(outputFileName: string, invoices: IInvoice[]) {
     const stream = fs.createWriteStream(outputFileName, "utf-8");
 
-    // Escreve o cabeçalho do CSV
     stream.write("NomeCliente;EnderecoCompleto;ValorFatura;NumeroPaginas\n");
 
     for (const invoice of invoices) {
-      const enderecoCompleto = `${invoice.CEP}, ${invoice.RuaComComplemento}, ${invoice.Cidade}, ${invoice.Estado}`;
+      const enderecoCompleto = `${
+        invoice.CEP
+      }, ${invoice.RuaComComplemento.trim()}, ${invoice.Cidade}, ${
+        invoice.Estado
+      }`;
       const valorFatura = parseFloat(String(invoice.ValorFatura)).toFixed(2);
-      const numeroPaginasPar =
-        invoice.NumeroPaginas % 2 === 0
-          ? invoice.NumeroPaginas
-          : invoice.NumeroPaginas + 1;
 
       stream.write(
-        `${invoice.NomeCliente};${enderecoCompleto};${valorFatura};${numeroPaginasPar}\n`
+        `${invoice.NomeCliente};${enderecoCompleto.trim()};${valorFatura};${
+          invoice.NumeroPaginas
+        }\n`
       );
     }
 
     stream.end();
   }
-
-  filtrarArquivoDeTexto(
-    nomeArquivoEntrada: string,
-    nomeArquivoSaida: string
-  ): void {
-     this.readTextFile(nomeArquivoEntrada, nomeArquivoSaida);
-  }
 }
 
 const nomeArquivo = "base_hi.txt"; // Substitua pelo nome do seu arquivo
-new ProcessFile().filtrarArquivoDeTexto(nomeArquivo, "saida.csv");
+new ProcessFile().readTextFile(nomeArquivo, "saida.csv");
